81c0da3ea8f6f3a36418f8665a40e3f8097e237a
Роман <romanze1706@gmail.com>
2022-11-14 20:43:47 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         int[] weight = new int[]{1, 2, 3};
-        for (int i = 0; i < weight.length; i++) {
+        for (int i = weight.length - 1; i >=0; i--) {
             System.out.print(weight[i]);
-            if (i != weight.length - 1) {
+            if (i != 0) {
                 System.out.print(",");
             }
 
         }
         System.out.println();
         double[] soul = {1.57, 7.654, 9.986};
-        for (int i = 0; i < soul.length; i++) {
+        for (int i = soul.length -1; i >=0; i--) {
             System.out.print(soul[i]);
-            if (i != soul.length - 1) {
+            if (i != 0) {
                 System.out.print(",");
             }
 
         }
         System.out.println();
         int[] dog = {3, 5, 7, 8, 546};
-        for (int i = 0; i < dog.length; i++) {
+        for (int i = dog.length - 1; i >= 0; i--) {
             System.out.print(dog[i]);
-            if (i != dog.length -1) {
+            if (i != 0) {
                 System.out.print(",");
             }
 
