dcbf9a9cb1d46f31b8bcb809ac3dea088d022c27
Роман <romanze1706@gmail.com>
2022-11-14 17:31:58 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] weight = new int[]{1, 2, 3};
+        double[] soul = {1.57, 7.654, 9.986};
+        int [] dog = {3,5,7,8,546};
+
+
+
     }
 }
