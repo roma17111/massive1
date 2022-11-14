e21a34b6be37ed29ef9341006d968595cfae8d73
Роман <romanze1706@gmail.com>
2022-11-14 19:24:09 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         int[] weight = new int[]{1, 2, 3};
+       int a = weight[0];
+       int b =weight[1];
+       int c = weight[2];
+        System.out.println(a + "," + b + "," + c);
         double[] soul = {1.57, 7.654, 9.986};
+        double a1 = soul[0]; double a2 =soul[1]; double a3 = soul[2];
+        System.out.println(a1 + "," + a2 + "," + a3);
         int [] dog = {3,5,7,8,546};
+        int b1 = dog[0]; int b2 = dog[1];
+        int b3 = dog[2]; int b4 = dog[3];
+        int b5 = dog[4];
+        System.out.println(b1 + "," + b2 + "," + b3 + "," + b4 + "," + b5 );
+
 
 
 
